fe821d540f843fd052a448e6269f500344172642
Hack Reactor Students <accounts@hackreactor.com>
2015-09-24 15:36:22 -0700

    implement server-side skeleton

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -8,12 +8,15 @@ module.exports = function (app, express) {
   var userRouter = express.Router();
   var linkRouter = express.Router();
 
+  //morgan is for logging get and post data to the console.
   app.use(morgan('dev'));
+  //bodyParser is for processing body req information (e.g., req.body)
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  //serving all of the static files from the client directory
   app.use(express.static(__dirname + '/../../client'));
 
-
+  //TODO change all the routers. 
   app.use('/api/users', userRouter); // use user router for all user request
 
   // authentication middleware used to decode token and made available on the request
@@ -23,6 +26,7 @@ module.exports = function (app, express) {
   app.use(helpers.errorHandler);
 
   // inject our routers into their respective route files
+  //TODO this will need to change to reflect our new routers
   require('../users/userRoutes.js')(userRouter);
   require('../links/linkRoutes.js')(linkRouter);
 };
