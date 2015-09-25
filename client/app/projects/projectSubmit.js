516881fb2d851c7ed3355f9927ede3632ec6d2fb
Hack Reactor Students <accounts@hackreactor.com>
2015-09-25 15:18:51 -0700

    define routes on client side, create project submission form, projects display table

--- client/app/projects/projectSubmit.js
+++ client/app/projects/projectSubmit.js
@@ -1,20 +1,33 @@
 angular.module('drone.projects', [])
 
 .controller('SubmitProjectController', function ($scope, $location, ProjectFactory) {
-
+  //for the adding of the project
   $scope.project = {};
-  //write a function that adds a data-scope property to the
+  //for the retriving of all the projects
+  $scope.data = {};
 
-
+  //A function to add a project to our database
   $scope.addProject = function () {
-    $scope.loading = true;
+    // $scope.loading = true;
     ProjectFactory.addProject($scope.project)
       .then(function () {
-        $scope.loading = false;
-        $location.path('/');
+        // $scope.loading = false;
+        // $location.path('/');
       })
       .catch(function (error) {
         console.log(error);
       });
   };
+
+  //Add a function to get all existing projects from the database
+
+  $scope.getProjects = function () {
+    ProjectFactory.getProjects()
+      .then(function (projects) {
+        $scope.data.openProjects = projects; 
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } 
   });
