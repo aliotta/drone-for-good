a4a7e6ab53335998ad2e918f6328cc3968b08770
MattLoftus <matthew.a.loftus@gmail.com>
2015-09-30 11:25:01 -0700

    move map initialization into map controller

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -1,18 +1,44 @@
 angular.module('drone.map', [])
 
-.controller('MapController', function ($scope, $location, mapFactory) {
-  // Your code here
+.controller('MapController', function ($scope, $location) {
+  //Get project data
 
-  // $scope.link = {};
-  // $scope.addLink = function () {
-  //   $scope.loading = true;
-  //   Links.addLink($scope.link)
-  //     .then(function () {
-  //       $scope.loading = false;
-  //       $location.path('/');
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     });
-  // };
+  //Initialize map object and add to dom
+  $scope.initialize = function () {
+        //Grab map element in DOM and specify options
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+          center: new google.maps.LatLng(37.789, -122.416),
+          zoom: 9,
+          mapTypeId: google.maps.MapTypeId.HYBRID
+        }
+        //Make new instance of google maps object
+        var map = new google.maps.Map(mapCanvas, mapOptions)
+
+        //Attach all no fly zone geoJSON data 
+        map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/5_mile_airport.geojson');
+        map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/us_military.geojson');
+        map.data.loadGeoJson('https://raw.githubusercontent.com/mapbox/drone-feedback/master/sources/geojson/us_national_park.geojson');
+
+        //Set main styles for no fly zone data
+        map.data.setStyle({
+          fillOpacity: .5,
+          strokeWeight: 2,
+          fillColor: 'red'
+        });
+
+        //Iterate over testMarkers and add a marker for each one.
+        //Pass addMarker a latLng, map instance, and infowindow instance
+        // for (var i = 0; i < testMarkers.length; i++) {
+        //   var location = testMarkers[i];
+        //   addMarker(location, map, makeInfoWindow(contentArray[i]) );
+        // }
+
+      }
+
+      //When page loads, create the map
+      google.maps.event.addDomListener(window, 'load', $scope.initialize);
+    //Inject project data into DOM
+    //
+
   });
