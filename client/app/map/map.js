955d59b402ae055dcf24f848e1ff259465ec9ce3
MattLoftus <matthew.a.loftus@gmail.com>
2015-09-30 13:57:50 -0700

    Markers reflect coordinates and descriptions of projects in the database

--- client/app/map/map.js
+++ client/app/map/map.js
@@ -6,6 +6,56 @@ angular.module('drone.map', [])
   $scope.locations = [];
   $scope.descriptions = [];
 
+  //Set image url for marker, and change orgin/anchor so 
+  //flagpole bottom centered on marker coordinates
+  var image = {
+    url: 'tiny_green_flag',
+    //size: new google.maps.Size(20, 32),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(0, 32)
+  };
+
+  //A method to get project data from project factory, and populate
+  //scope properties with coordinates and descriptions.
+  $scope.getProjects = function() {
+    ProjectFactory.getProjects()
+    .then(function (projectData) {
+      for (var i = 0; i < projectData.length; i++) {
+        var currentEntry = projectData[i];
+        $scope.locations[i] = {
+          lat: currentEntry.latitude, 
+          lng: currentEntry.longitude
+        };
+        $scope.descriptions[i] = '<p>' + currentEntry.description + '</p>'
+      }
+    });
+  };
+
+  //Invoke getProjects to populate our locations and descriptions arrays
+  $scope.getProjects();
+
+  //A function for creating a new info window.  
+  $scope.makeInfoWindow = function (contentString) {
+    var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 200
+    });
+    return infowindow;
+  }
+
+  //Function for adding markers to map object
+  $scope.addMarker = function (location, map, infowindow) {
+    var marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: image
+    });
+    marker.addListener('click', function () {
+      infowindow.open(map, marker);
+    })
+  };
+
   //Initialize map object and add to dom
   $scope.initialize = function () {
         //Grab map element in DOM and specify options
@@ -30,32 +80,18 @@ angular.module('drone.map', [])
           fillColor: 'red'
         });
 
-        //Iterate over testMarkers and add a marker for each one.
-        //Pass addMarker a latLng, map instance, and infowindow instance
-        // for (var i = 0; i < testMarkers.length; i++) {
-        //   var location = testMarkers[i];
-        //   addMarker(location, map, makeInfoWindow(contentArray[i]) );
-        // }
-
-  }
-  $scope.getProjects = function() {
-    ProjectFactory.getProjects();
-    .then(function (projectData) {
-      for (var i = 0; i < projectData.length; i++) {
-        var currentEntry = projectData[i];
-        $scope.locations[i] = {
-          lat: currentEntry.latitude, 
-          lng: currentEntry.longitude
-        };
-        $scope.descriptions[i] = '<p>' + currentEntry.description + '</p>'
-      }
-    });
+        // Iterate over testMarkers and add a marker for each one.
+        // Pass addMarker a latLng, map instance, and infowindow instance
+        for (var i = 0; i < $scope.locations.length; i++) {
+          var location = $scope.locations[i];
+          $scope.addMarker(location, map, $scope.makeInfoWindow($scope.descriptions[i]) );
+        }
 
   }
 
-      //When page loads, create the map
-      google.maps.event.addDomListener(window, 'load', $scope.initialize);
-    //Inject project data into DOM
-    //
+  //When page loads, create the map
+  google.maps.event.addDomListener(window, 'load', $scope.initialize);
+});
 
-  });
+
+
